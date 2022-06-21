5a4ab648767a9962e4dea69de1c7f1e9b863c057
MustafaSalihEybek <53754841+MustafaSalihEybek@users.noreply.github.com>
2022-06-21 21:49:12 +0300

    SignInViewModel oluşturuldu

---
+++ app/src/main/java/com/nexis/obuluyor/view/SignInViewModel.kt
@@ -0,0 +1,7 @@
+package com.nexis.obuluyor.view
+
+import android.app.Application
+import com.nexis.obuluyor.viewmodel.base.BaseViewModel
+
+class SignInViewModel(application: Application) : BaseViewModel(application) {
+}
