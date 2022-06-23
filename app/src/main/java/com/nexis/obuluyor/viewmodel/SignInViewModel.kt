c417b035e0cd4eea7721292c5e0f849356e6a393
MustafaSalihEybek <53754841+MustafaSalihEybek@users.noreply.github.com>
2022-06-23 16:19:34 +0300

    Giriş yaparken arkaplanda olan yapılar bu sınıfa yazıldı

--- app/src/main/java/com/nexis/obuluyor/viewmodel/SignInViewModel.kt
+++ app/src/main/java/com/nexis/obuluyor/viewmodel/SignInViewModel.kt
@@ -1,7 +1,42 @@
 package com.nexis.obuluyor.viewmodel
 
 import android.app.Application
+import androidx.lifecycle.MutableLiveData
+import com.nexis.obuluyor.model.Sign
+import com.nexis.obuluyor.model.User
+import com.nexis.obuluyor.repository.SignInRepository
+import com.nexis.obuluyor.util.AppUtils
 import com.nexis.obuluyor.viewmodel.base.BaseViewModel
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.observers.DisposableSingleObserver
+import io.reactivex.schedulers.Schedulers
 
 class SignInViewModel(application: Application) : BaseViewModel(application) {
+    val successMessage = MutableLiveData<String>()
+    val errorMessage = MutableLiveData<String>()
+
+    fun signInUser(email: String, password: String){
+        AppUtils.signInRepository = SignInRepository()
+        AppUtils.disposable = CompositeDisposable()
+
+        AppUtils.disposable.add(
+            AppUtils.signInRepository.signInUser(email, password)
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(object : DisposableSingleObserver<Sign>(){
+                    override fun onSuccess(t: Sign) {
+                        if (t.result.message != null)
+                            errorMessage.value = t.result.message
+
+                        if (t.user.ad_soyad != null)
+                            successMessage.value = "Başarıyla giriş yaptınız"
+                    }
+
+                    override fun onError(e: Throwable) {
+                        errorMessage.value = e.localizedMessage
+                    }
+                })
+        )
+    }
 }
