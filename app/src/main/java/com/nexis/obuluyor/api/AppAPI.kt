96e211955441e069d05d8bf3259b21984a894188
MustafaSalihEybek <53754841+MustafaSalihEybek@users.noreply.github.com>
2022-06-23 16:13:11 +0300

    Yeni repository yapıları için gerekli yapılar hazırlandı

--- app/src/main/java/com/nexis/obuluyor/api/AppAPI.kt
+++ app/src/main/java/com/nexis/obuluyor/api/AppAPI.kt
@@ -1,13 +1,35 @@
 package com.nexis.obuluyor.api
 
-import com.nexis.obuluyor.model.Category
+import com.nexis.obuluyor.model.*
 import io.reactivex.Single
-import okhttp3.RequestBody
-import okhttp3.ResponseBody
-import retrofit2.Response
 import retrofit2.http.*
 
 interface AppAPI {
     @POST("ana_kategori.php")
     fun getCategories() : Single<List<Category>>
+
+    @POST("iller.php")
+    fun getCountries() : Single<List<Country>>
+
+    @POST("ilceler.php")
+    @FormUrlEncoded
+    fun getCities(@Field("il") countryId: Int) : Single<List<City>>
+
+    @POST("uye_ol.php")
+    @FormUrlEncoded
+    fun signUpUser(
+        @Field("ad_soyad") fullName: String,
+        @Field("dogum_tarihi") birthday: String,
+        @Field("cinsiyet") gender: String,
+        @Field("eposta") email: String,
+        @Field("parola") password: String,
+        @Field("telefon") phone: String,
+        @Field("gsm") gsm: String,
+        @Field("il") country: Int,
+        @Field("ilce") city: Int
+    ) : Single<Sign>
+
+    @POST("kullanici_girisi.php")
+    @FormUrlEncoded
+    fun signInUser(@Field("eposta") email: String, @Field("sifre") password: String) : Single<Sign>
 }
